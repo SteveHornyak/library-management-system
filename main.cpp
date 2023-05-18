da0431692b39aca9ce7bf74056fd327fd0501005
Steve Hornyak <sphornyak4@gmail.com>
2023-05-18 15:15:32 -0700

    Added more functionalities

--- main.cpp
+++ main.cpp
@@ -7,6 +7,21 @@ using std::cin;
 using std::endl;
 using std::string;
 
+void patron()
+{
+	cout << "Please select an option:\n\t(1) Check out item\n\t(2) Renew an item\n\t(3) See due date of items\n>>";
+
+	string s = "";
+	int opt = -1;
+
+	cin >> s;
+}
+
+void librarian()
+{
+
+}
+
 int main(int argc, char* argv[])
 {
 	cout << "Welcome to the Hornyak Library!\n" << endl;
@@ -14,10 +29,10 @@ int main(int argc, char* argv[])
 	string input = "";
 	int iput = -1;
 
+	cout << "Please choose one of the following numbers. Are you a:\n\t(1) Patron\n\t(2) Librarian" << endl;
+
 	while(1)
 	{
-		cout << "Please choose one of the following numbers. Are you a:\n\t(1) Patron\n\t(2) Librarian" << endl;
-
 		
 		cout << ">> ";
 		cin >> input;
@@ -25,15 +40,25 @@ int main(int argc, char* argv[])
 		if(atoi(input.c_str()) == 1 || atoi(input.c_str()) == 2)
 		{
 			iput = atoi(input.c_str());
+
+			if(iput == 1)
+			{
+				//Go to the patron functions
+				patron();
+			}
+			else if(iput == 2)
+			{
+				//GO to the librarian functions
+				librarian();
+			}
+
 			break;
 		}
 		else
 		{
-			cout << "Invalid option. Please try again.\n" << endl;
+			cout << "Invalid option. Please try again.\n";
 		}
 	}
-
-	cout << iput << endl;
 
 
 	cout << "Thanks for visiting your local library! Have a great day!" << endl;	
